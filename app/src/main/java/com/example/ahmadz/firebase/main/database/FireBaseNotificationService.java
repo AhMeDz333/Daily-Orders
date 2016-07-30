d71fbf2925a0d86064771eca7a9dfcd0ea3b1efe
ahmadz <ahmed.ashraf33@yahoo.com>
2016-07-30 21:19:58 +0200

    Added firebase notification service.

--- app/src/main/java/com/example/ahmadz/firebase/main/database/FireBaseNotificationService.java
+++ app/src/main/java/com/example/ahmadz/firebase/main/database/FireBaseNotificationService.java
@@ -4,8 +4,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -66,13 +64,13 @@ public class FireBaseNotificationService extends FirebaseMessagingService {
 		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
 				PendingIntent.FLAG_ONE_SHOT);
 
-		Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
-				R.drawable.sandwich);
+//		Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
+//				R.drawable.sandwich);
 
 		Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
 				.setSmallIcon(R.mipmap.ic_launcher)
-				.setLargeIcon(largeIcon)
+//				.setLargeIcon(largeIcon)
 				.setContentTitle("Daily Orders")
 				.setContentText(messageBody)
 				.setAutoCancel(true)
